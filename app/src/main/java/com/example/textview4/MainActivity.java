694174a3402fd6285686f7b515b100f654c8487c
ad2553 <ad2553@bs.amalnet.k12.il>
2023-12-24 12:06:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/textview4/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.textview4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
+        btn = findViewById(R.id.btn);
+
+    }
+int count = 1;
+    public void go(View view) {
+        tv.setText("this is a click number:"+count);
+        tv.setTextColor(Color.GREEN);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                if ((count&10) == 7){
+                    tv.setText("Boom!");
+                }
+                else{
+                    tv.setText("this is a click number:"+count);
+                }
+            }
+        });
+
+
+    }
+}
